e3fac8114735619934e8c1ff51d2b05e61b41fc4
Liudmila <bauklu@mail.ru>
2022-05-03 13:32:25 +0300

    task splution changed

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Given matrix where you have to find cats by ears "^^"
